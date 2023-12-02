57658b6381ec77bcc0767050b363fdb247ad41b8
ПК <youngcocumber@gmail.com>
2023-12-02 12:57:26 +0800

    н

--- app/src/main/java/ru/dolbak/blackjack/MainActivity.java
+++ app/src/main/java/ru/dolbak/blackjack/MainActivity.java
@@ -3,9 +3,11 @@ package ru.dolbak.blackjack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     void putCard(int slot, Card card){
         int[] slots = {R.id.dealer_card_4, R.id.dealer_card_5, R.id.dealer_card_3,
@@ -26,29 +28,28 @@ public class MainActivity extends AppCompatActivity {
         putCard(0,card);
         Card card1 = deck.take();
         card1.rate.getPoints();
-        putCard(1,card1);
         Card card2 = deck.take();
         Card card3 = deck.take();
         Card card4 = deck.take();
-        Card[] cards = {card,card1,card2,card3,card4};
+        Card card5 = deck.take();
+        Card card6 = deck.take();
+//        Card card7 = deck.take();
+        Card[] cards = {card,card1,card2,card3,card4,card5,card6};
         int sum = card.rate.getPoints()+card1.rate.getPoints();
-        int i=2;
-        while (sum < 16){
-            putCard(i, cards[i]);
+        int i=1;
+        while (sum < 17){
             sum += cards[i].rate.getPoints();
             i++;
         }
         putCard(5,deck.take());
         putCard(6,deck.take());
+    }
+    int takeCard = 7;
+    public void TakeCard(View view, Card[] cards)
+    {
 
-
-
-
-
-        //TODO: используйте putCard(номер слота, карта), чтобы отрисовать карту на столе
-
-
-        Card myCard = new Card(Rate.QUEEN, Suit.Hearts);
-        int p = myCard.rate.getPoints(); //таким образом можно получить "очки" карты
+        Deck deck = new Deck();
+        putCard(7,deck.take());
+        takeCard++;
     }
 }
